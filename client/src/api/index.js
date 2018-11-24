f25d3e84fcdff829e92c5441dcfbb824f9ee44c8
ulydev <ulydev@live.fr>
2018-11-24 21:19:41 +0100

    remove console log

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -21,7 +21,6 @@ const processHeads = (image) => {
       for (let i = 0; i < data.length; ++i) {
         const currentEmotions = data[i].faceAttributes.emotion;
         for (const emotionName in currentEmotions) {
-          console.log(emotionName);
           if (!(emotionName in emotions)) {
             emotions[emotionName] = 0; // initialize emotion
           }
