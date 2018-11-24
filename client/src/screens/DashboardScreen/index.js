876eb84aad06ae01179e5fd0e2512cdf3082eacf
ulydev <ulydev@live.fr>
2018-11-25 00:17:10 +0100

    get max emotion

--- client/src/screens/DashboardScreen/index.js
+++ client/src/screens/DashboardScreen/index.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 
 import { processHands, processHeads } from '../../api';
-import DashBar from "../../components/DashBar"
 
+import FullscreenEmotion from '../../components/FullscreenEmotion';
+import DashBar from '../../components/DashBar';
 import VideoFeed from '../../components/VideoFeed';
 
+const getMaxEmotion = (emotions) => {
+    let maxEmotion = null;
+    for (const emotionName in emotions) {
+        if (!maxEmotion || emotions[maxEmotion] < emotions[emotionName]) {
+            maxEmotion = emotionName;
+        }
+    }
+    return maxEmotion;
+}
+
 class DashboardScreen extends Component {
+    state = {
+        data: {}
+    };
 
     onGotPicture = (blob) => {
         const { onGotNewData } = this.props;
@@ -15,17 +29,28 @@ class DashboardScreen extends Component {
             processHeads(blob)
         ])
         .then(([hands, heads]) => {
-            onGotNewData({ hands, heads });
+            const data = { hands, heads };
+            onGotNewData(data);
             this.videoFeed.startTimer();
+
+            this.setState({
+                data,
+                emotion: getMaxEmotion(heads.emotions)
+            });
         })
         .catch(console.error);
+    }
+
+    flash = () => {
+
     }
 
     render() {
         return (
             <div className="fullscreen">
+                <FullscreenEmotion emotion={this.state.emotion} />
                 <VideoFeed ref={ref => this.videoFeed = ref} onGotPicture={this.onGotPicture} pictureDelay={500} />
-                <DashBar peopleCount={400} raisingHand={50}/>
+                <DashBar peopleCount={400} raisingHand={50} />
             </div>
         );
     }
