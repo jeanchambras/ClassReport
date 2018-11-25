f7eba6dc2e85c0b9a191bfc4bc97add2547cbe89
ulydev <ulydev@live.fr>
2018-11-25 06:07:46 +0100

    graph

--- client/src/screens/ChartScreen/index.js
+++ client/src/screens/ChartScreen/index.js
@@ -1,26 +1,63 @@
 import React, { Component } from 'react';
-import {Line} from 'react-chartjs-2';
+
+import ReactChartkick, { LineChart, PieChart, ScatterChart } from 'react-chartkick';
+import Chart from 'chart.js';
+ReactChartkick.addAdapter(Chart);
+
+/*
+    data = [
+        {
+            time: ...,
+            data: {
+                heads: {
+                    count,
+                    emotions: {
+                        anger: 0.55,
+                        ...
+                    }
+                }
+            }
+        },
+        {
+            ...
+        },
+        ...
+    ]
+*/
+const computeTotalEmotions = (data) => {
+    let emotionValues = {};
+    for (const i in data) {
+        const currentData = data[i];
+        const emotions = currentData.data.heads.emotions;
+        for (const emotionName in emotions) {
+            if (emotions[emotionName] > 0) {
+                if (!emotionValues[emotionName]) {
+                    emotionValues[emotionName] = 0;
+                }
+                emotionValues[emotionName] += emotions[emotionName];
+            }
+        }
+    }
+    return Object.keys(emotionValues).map(emotionName => [emotionName, emotionValues[emotionName]]);
+}
 
 class ChartScreen extends Component {
-
-    state = {
-        data: [{
-            x: 10,
-            y: 20
-        }, {
-            x: 15,
-            y: 10
-        }]
-    };
-    
-    render() {
-        const { data } = this.state;
-        return (
-            <div className="main">
-                <Line data={data}/>
+	render() {
+        const data = this.props.data;
+		return (
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+                <h1><b>Session results</b></h1>
+                <div style={{ width: 800 }}>
+                    { /* <h2>First Chart</h2> */ }
+                    <LineChart data={data.map(v => [v.time, v.data.heads.count])} xtitle="Time" ytitle="Attentive students" />
+                </div>
+                <div style={{ width: 800 }}>
+                    { /* <h2>Second Chart</h2> */ }
+                    <PieChart data={computeTotalEmotions(data)} />
+                </div>
             </div>
         );
-    }
+	}
 
 }
 
