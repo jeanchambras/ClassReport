e5aa73d99227bfe0f3848ce2207a4a99cb52e9af
jean <jean.chambras@epfl.ch>
2018-11-25 01:14:54 +0100

    Add logo

--- client/src/screens/StartScreen/index.js
+++ client/src/screens/StartScreen/index.js
@@ -14,8 +14,11 @@ class StartScreen extends Component {
             <div className={"hero is-fullheight"}>
                 <div className="hero-body has-background-primary ">
                     <div className="container">
-                        <div className="columns is-full">
+                        <div className="columns is-full is-multiline has-text-centered">
                             <div className="column has-text-centered is-12">
+                                <img src={require("../../assets/logo.svg")} className="is-logo"/>
+                            </div>
+                            <div className="column is-12">
                                 <Button onClick={ onStart }>
                                     Start
                                 </Button>
